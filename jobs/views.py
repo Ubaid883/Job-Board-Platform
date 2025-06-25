be3cda25122b505ca5d41912f46546ae6bbe0beb
ubaid <ubaidsaleem825@gmail.com>
2025-06-25 09:30:17 +0500

    migrate data

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, filters
+from .models import JobListing
+from .serializer import JobListingSerializer
 
+class JobListingView(generics.ListAPIView):
+    queryset = JobListing.objects.filter(is_active=True)
+    serializer_class = JobListingSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title','location','description']
 # Create your views here.
