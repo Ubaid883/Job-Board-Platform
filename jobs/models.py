501d36265f71c692b0465e5c11085018faad889d
ubaid <ubaidsaleem825@gmail.com>
2025-06-25 09:10:27 +0500

    create jobs models

---
+++ jobs/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# Employee Model
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=50)
+    description = models.TextField()
+    
+class Candidate(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    resume = models.FileField(upload_to='resume/')
+    skills = models.TextField()
+    
+# Job Listing Model
+class JobListing(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+# Application Model
+class JobApplication(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    job = models.ForeignKey(JobListing, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=[
+           ('Applied', 'Applied'),
+        ('Reviewed', 'Reviewed'),
+        ('Rejected', 'Rejected'),
+        ('Accepted', 'Accepted'),
+        ], default='Applied')
